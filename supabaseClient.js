4b0da274f1ffe1d545c1383a4761f91b876210f4
shabeer <duagrandevents@gmail.com>
2026-02-08 23:55:19 +0530

    Remove all files per user request

--- supabaseClient.js
+++
@@ -1,7 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://vttebwaiipmhbtrkveka.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ0dGVid2FpaXBtaGJ0cmt2ZWthIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAwNTYyMTUsImV4cCI6MjA4NTYzMjIxNX0.Qj3M9FCnm9HRxkxZ779i64yPjnxDXXqyRN-2htyHdv4'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
